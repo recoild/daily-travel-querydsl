640c64e5d489c48daeec665d81d5cb2a34c3287e
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2024-08-13 11:27:44 +0900

    feat: ApiResponse 추가

---
+++ src/main/java/com/fisa/dailytravel/global/dto/ApiResponse.java
@@ -0,0 +1,44 @@
+package com.fisa.dailytravel.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class ApiResponse<T> {
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    // 반환 데이터 없이 성공했을 때
+    public static <T> ApiResponse<T> ok() {
+        return (ApiResponse<T>) ApiResponse.builder()
+                .status(HttpStatus.OK)
+                .message("성공")
+                .data(null)
+                .build();
+    }
+
+    // 반환 데이터 있고 성공했을 때
+    public static <T> ApiResponse<T> ok(T data) {
+        return (ApiResponse<T>) ApiResponse.builder()
+                .status(HttpStatus.OK)
+                .message("성공")
+                .data(data)
+                .build();
+    }
+
+    // 실패했을 때
+    public static <T> ApiResponse<T> error(T errorCode) {
+        return (ApiResponse<T>) ApiResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("실패")
+                .data(errorCode)
+                .build();
+    }
+}
