bae44c8a860d70250ada7518ee6aece754d9f6b8
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2024-08-14 14:11:41 +0900

    feat: 게시글 상세 조회 메소드 수정 및 Post의 작성일 형변환

--- src/main/java/com/fisa/dailytravel/post/service/PostServiceImpl.java
+++ src/main/java/com/fisa/dailytravel/post/service/PostServiceImpl.java
@@ -1,8 +1,11 @@
 package com.fisa.dailytravel.post.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.fisa.dailytravel.global.config.S3Uploader;
 import com.fisa.dailytravel.post.dto.PostRequest;
 import com.fisa.dailytravel.post.dto.PostResponse;
 import com.fisa.dailytravel.post.models.Hashtag;
+import com.fisa.dailytravel.post.models.Image;
 import com.fisa.dailytravel.post.models.Post;
 import com.fisa.dailytravel.post.models.PostHashtag;
 import com.fisa.dailytravel.post.repository.HashTagRepository;
@@ -13,9 +16,13 @@ import com.fisa.dailytravel.user.models.User;
 import com.fisa.dailytravel.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDate;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,32 +33,37 @@ public class PostServiceImpl implements PostService {
     private final ImageRepository imageRepository;
     private final HashTagRepository hashTagRepository;
     private final PostHashtagRepository postHashtagRepository;
+    private final S3Uploader s3Uploader;
+    private final AmazonS3 s3Client;
 
     @Override
-    public String savePost(String uuid, PostRequest postRequest) {
+    public String savePost(String uuid, PostRequest postRequest) throws IOException {
         User user = userRepository.findByUuid(uuid);
 
+        // TODO: thumbnail 경로 이름 수정
         Post post = Post.builder()
                 .title(postRequest.getTitle())
                 .content(postRequest.getContent())
                 .placeName(postRequest.getPlaceName()).likesCount(0)
-//                .thumbnail(postRequest.getImageFiles().get(0))
+                .thumbnail(postRequest.getImageFiles().get(0).getOriginalFilename())
                 .latitude(postRequest.getLatitude())
                 .longitude(postRequest.getLongitude())
-                .createdAt(LocalDate.now())
+//                .createdAt(LocalDate.now())
                 .user(user)
                 .build();
 
         postRepository.save(post);
 
-//        List<String> imageFiles = postRequest.getImageFiles();
+        List<MultipartFile> imageFiles = postRequest.getImageFiles();
 
-//        for (String imageFile : imageFiles) {
-//            imageRepository.save(Image.builder()
-//                    .post(post)
-//                    .imagePath(imageFile)
-//                    .build());
-//        }
+        for (MultipartFile imageFile : imageFiles) {
+            String imageUrl = s3Uploader.uploadImage("post", user.getNickname(), post.getId(), imageFile);
+
+            imageRepository.save(Image.builder()
+                    .post(post)
+                    .imagePath(imageUrl)
+                    .build());
+        }
 
         List<String> hashtags = postRequest.getHashtag();
 
@@ -71,7 +83,18 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostResponse getPost(String uuid, Long postId) {
-        return PostResponse.of(postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글 없음")));
+        Optional<Post> post = postRepository.findById(postId);
+        List<Image> images = imageRepository.findByPostId(postId);
+
+        List<String> imageFiles = new ArrayList<>();
+
+        String path = post.get().getUser().getNickname() + "-postId" + postId + "-";
+
+        for (Image image : images) {
+            URL url = s3Client.getUrl("fisa-dailytravel-bucket-test", path + image.getImagePath());
+            imageFiles.add(url.toString());
+        }
+
+        return PostResponse.of(post.get(), imageFiles);
     }
 }
