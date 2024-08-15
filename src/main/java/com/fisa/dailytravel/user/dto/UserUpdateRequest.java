ad91161f7fb9616e60fcfd5bdb473d79a36dca6d
RamRam-V <cajhyung@gmail.com>
2024-08-15 16:01:56 +0900

    feat: user 수정 구현

---
+++ src/main/java/com/fisa/dailytravel/user/dto/UserUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.fisa.dailytravel.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@Builder
+public class UserUpdateRequest {
+    private String uuid;
+    private String nickname;
+    private MultipartFile profileImageFile;
+
+}
--- src/main/java/com/fisa/dailytravel/user/service/UserServiceImpl.java
+++ src/main/java/com/fisa/dailytravel/user/service/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.fisa.dailytravel.user.service;
 
+import com.fisa.dailytravel.global.config.S3Uploader;
 import com.fisa.dailytravel.user.dto.UserCreateRequest;
+import com.fisa.dailytravel.user.dto.UserUpdateRequest;
+import com.fisa.dailytravel.user.dto.UserUpdateResponse;
 import com.fisa.dailytravel.user.models.User;
 import com.fisa.dailytravel.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.Optional;
 
@@ -13,6 +18,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final S3Uploader s3Uploader;
 
     @Override
     public void signin(UserCreateRequest userCreateRequest) throws Exception {
@@ -32,7 +38,30 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUser(String uuid) {
+    public User getUser(String uuid) throws Exception {
         return userRepository.findByUuid(uuid);
+    }
+
+    @Override
+    public UserUpdateResponse updateUser(UserUpdateRequest userUpdateRequest) throws IOException {
+        User user = userRepository.findByUuid(userUpdateRequest.getUuid());
+        Optional<User> userOptional = Optional.ofNullable(user);
+        if (userOptional.isEmpty())
+            return null;
+
+        user.setNickname(userUpdateRequest.getNickname());
+        MultipartFile imageFile = userUpdateRequest.getProfileImageFile();
+        Optional<MultipartFile> imageFileOptional = Optional.ofNullable(imageFile);
+        if (imageFileOptional.isPresent()) {
+            String imageUrl = s3Uploader.uploadImage("post", user.getNickname(), user.getId(), imageFile);
+        }
+
+        userRepository.save(user);
+
+
+        return UserUpdateResponse.builder()
+                .nickname(user.getNickname())
+                .profileImagePath(user.getProfileImagePath())
+                .build();
     }
 }
