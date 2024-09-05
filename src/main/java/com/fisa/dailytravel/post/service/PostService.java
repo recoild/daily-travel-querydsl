f23e631793e1f9d9b23889b276bdab54549be275
RamRam-V <cajhyung@gmail.com>
2024-09-05 23:41:49 +0900

    fix: comment mapper 오류 수정

--- src/main/java/com/fisa/dailytravel/post/service/PostService.java
+++ src/main/java/com/fisa/dailytravel/post/service/PostService.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public interface PostService {
     String savePost(String uuid, PostRequest postRequest) throws IOException;
 
-    PostResponse getPost(String uuid, Long postId, CommentPageRequest commentPageRequest);
+    PostResponse getPost(String uuid, Long postId);
 
     PostPagingResponse getAllPosts(String uuid, PostPagingRequest postPagingRequest);
 
