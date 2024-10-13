3943ecc92f5b9dfaf068cf3bdabb8add220ac9c7
recoild <cajhyung@gmail.com>
2024-10-13 22:53:55 +0900

    fix: postman 에서 image 첨부 시 tmp 파일을 업로드 하여 발생하는 오류를 catch 하고 무시.

--- src/main/java/com/fisa/dailytravel/post/service/AsyncImageUploadService.java
+++ src/main/java/com/fisa/dailytravel/post/service/AsyncImageUploadService.java
@@ -5,9 +5,11 @@ import com.fisa.dailytravel.post.models.Image;
 import com.fisa.dailytravel.post.models.Post;
 import com.fisa.dailytravel.post.repository.ImageRepository;
 import com.fisa.dailytravel.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Service
+@RequiredArgsConstructor
 public class AsyncImageUploadService {
     private final S3Uploader s3Uploader;
     private final ImageRepository imageRepository;
@@ -22,35 +25,35 @@ public class AsyncImageUploadService {
 
     private final String imageDirectory = "post";
 
-    public AsyncImageUploadService(S3Uploader s3Uploader, ImageRepository imageRepository, PostRepository postRepository) {
-        this.s3Uploader = s3Uploader;
-        this.imageRepository = imageRepository;
-        this.postRepository = postRepository;
-    }
-
     @Async("customTaskExecutor")
-    public CompletableFuture<String> uploadImagesAsync(List<MultipartFile> imageFiles, Post post) throws Exception {
+    @Transactional
+    public CompletableFuture<String> uploadImagesAsync(List<MultipartFile> imageFilesData, Post post) throws Exception {
+        System.out.println("Executing method asynchronously - " + Thread.currentThread().getName());
+        System.out.println("Image upload count: " + imageFilesData.size());
         List<Image> images = new ArrayList<>();
         int i = 0;
-        for (MultipartFile imageFile : imageFiles) {
+        for (MultipartFile imageFile : imageFilesData) {
+            try {
+                String imageUrl = s3Uploader.uploadImage(imageDirectory, imageFile);
 
-            System.out.println("Executing method asynchronously - " + Thread.currentThread().getName());
+                images.add(Image.builder()
+                        .imageNo(i++)
+                        .postId(post.getId())
+                        .imagePath(imageUrl)
+                        .build());
+            } catch (Exception e) {
+                // Postman 에서만 발생하는 오류는 아래와 같다.
+                // Postman 오류 : java.nio.file.NoSuchFileException: ~~~~.tmp
+                // 해당 오류를 catch 하여 로그 출력으로 처리.
 
-            String imageUrl = s3Uploader.uploadImage(imageDirectory, imageFile);
-//            Thread.sleep(3000);
-            images.add(Image.builder()
-                    .imageNo(i++)
-                    .postId(post.getId())
-                    .imagePath(imageUrl)
-                    .build());
+                e.printStackTrace();
+            }
         }
 
-        imageRepository.saveAll(images);
-
-        // 첫 번째 이미지를 썸네일로 설정
         if (!images.isEmpty()) {
             post.setThumbnail(images.get(0).getImagePath());
             postRepository.save(post);
+            imageRepository.saveAll(images);
         }
 
         return new AsyncResult<>("Image upload completed").completable();
