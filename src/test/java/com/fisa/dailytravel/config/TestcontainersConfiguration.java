09e9db1081e02334d6f7a1b37063b81fc5477641
recoild <cajhyung@gmail.com>
2024-10-15 10:40:12 +0900

    feat: 로컬 개발 환경 메모리 부족으로 Postgresql로 변경.

--- src/test/java/com/fisa/dailytravel/config/TestcontainersConfiguration.java
+++ src/test/java/com/fisa/dailytravel/config/TestcontainersConfiguration.java
@@ -1,14 +1,9 @@
 package com.fisa.dailytravel.config;
-
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.HostConfig;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -20,13 +15,19 @@ public class TestcontainersConfiguration {
 //                .withReuse(true);
 //    }
 
+//    @Bean
+//    @ServiceConnection
+//    MySQLContainer<?> mysqlContainer() {
+//        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"))
+//                .withReuse(true);
+//    }
+
+
     @Bean
     @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"))
-//                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
-//                        new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(3306), new ExposedPort(3306)))
-//                ))
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+                .withExposedPorts(5432)
                 .withReuse(true);
     }
 
@@ -34,9 +35,6 @@ public class TestcontainersConfiguration {
     @ServiceConnection(name = "redis")
     GenericContainer<?> redisContainer() {
         return new GenericContainer<>(DockerImageName.parse("redis:latest"))
-//                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
-//                        new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(6379), new ExposedPort(6379)))
-//                ))
                 .withExposedPorts(6379)
                 .withReuse(true);
 
